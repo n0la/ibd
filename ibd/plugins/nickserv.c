5365815ec1b1bd36c90c8ba35bf7d5e8c1c47813
Florian Stinglmayr <florian@n0la.org>
2017-11-16 17:54:28 +0100

    implement nick regain
    nickserv allows a nick to be regained. Implement that functionality in
    case we get error 433 (nick already in use).

--- ibd/plugins/nickserv.c
+++ ibd/plugins/nickserv.c
@@ -1,6 +1,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <stdbool.h>
 #include <getopt.h>
 
 #include <irc/message.h>
@@ -19,6 +20,11 @@ static void print_message(irc_message_t m)
     size_t linelen = 0;
     irc_error_t r;
 
+    if (m == NULL) {
+        fprintf(stderr, "nickserv: memory exhaustion\n");
+        return;
+    }
+
     r = irc_message_string(m, &line, &linelen);
 
     if (IRC_SUCCESS(r)) {
@@ -29,15 +35,43 @@ static void print_message(irc_message_t m)
     free(line);
 }
 
+static void send_regain(void)
+{
+    irc_message_t r = NULL;
+    char *altnick = NULL;
+
+    /* first change nick, and then regain
+     */
+    asprintf(&altnick, "%s_alt", nick);
+    if (altnick == NULL) {
+        fprintf(stderr, "nickserv: memory exhaustion\n");
+        return;
+    }
+
+    r = irc_message_make(nick, IRC_COMMAND_NICK, altnick, NULL);
+    print_message(r);
+
+    irc_message_free(r);
+    r = NULL;
+
+    /* regain command will either disconnect us, or change our nick
+     * to the correct one.
+     */
+    r = irc_message_privmsg(nick, "nickserv", "REGAIN %s %s", nick, pass);
+    print_message(r);
+
+    irc_message_free(r);
+    r = NULL;
+}
+
 static void send_register(void)
 {
     irc_message_t id = NULL;
     /* send register account
      */
-    id = irc_message_privmsg(nick, "nickserv", "IDENTIFY %s", pass);
-    if (id != NULL) {
-        print_message(id);
-    }
+    id = irc_message_privmsg(nick, "nickserv", "IDENTIFY %s %s", nick, pass);
+    print_message(id);
+
     irc_message_free(id);
     id = NULL;
 }
@@ -49,6 +83,7 @@ int main(int ac, char **av)
     irc_message_t m = NULL;
     irc_error_t r;
     int c = 0;
+    bool regain = false;
 
     static char const *optstr = "n:p:";
     static struct option opts[] = {
@@ -93,8 +128,13 @@ int main(int ac, char **av)
             continue;
         }
 
-        if (m->command != NULL && strcmp(m->command, "MODE") == 0) {
+        if (irc_message_is(m, IRC_COMMAND_MODE) && !regain) {
             send_register();
+        } else if (irc_message_is(m, IRC_ERR_NICKNAMEINUSE)) {
+            /* nick is in use, so identify and then try to regain
+             */
+            send_regain();
+            regain = true;
         }
     }
 
