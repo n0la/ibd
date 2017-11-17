388481f23612eff36a3367bc9c91b74846d46ad2
Florian Stinglmayr <florian@n0la.org>
2017-11-17 17:08:09 +0100

    fix regaining

--- ibd/plugins/nickserv.c
+++ ibd/plugins/nickserv.c
@@ -128,19 +128,13 @@ int main(int ac, char **av)
             continue;
         }
 
-        if (!irc_message_prefix_nick(m, nick)) {
-            /* we have no use for messages that are not directed at us
-             */
-            irc_message_free(m);
-            m = NULL;
-            continue;
-        }
-
         if (irc_message_is(m, IRC_COMMAND_MODE) && !regain) {
             send_register();
         } else if (irc_message_is(m, IRC_ERR_NICKNAMEINUSE)) {
+            fprintf(stderr, "nickserv: nick is in use, regaining...\n");
             /* nick is in use, so identify and then try to regain
              */
+            send_register();
             send_regain();
             regain = true;
         }
